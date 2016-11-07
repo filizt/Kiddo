191b92d351fd7aa3106ea6e4c94b1c156d0a97e0
Filiz Kurban <filiz@monahq.com>
2016-11-07 10:33:13 -0800

    Plist update for App Transport and TimeLineVC fix

--- Kiddo/TimelineViewController.swift
+++ Kiddo/TimelineViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TimelineViewController.swift
+//  TimeLineViewController.swift
 //  Kiddo
 //
 //  Created by Filiz Kurban on 11/7/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TimelineViewController: UIViewController {
+class TimeLineViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,4 +20,16 @@ class TimelineViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
