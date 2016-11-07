bfd2c77b70ca24b1020e39f1161ef1aaed209b31
Filiz Kurban <filiz@monahq.com>
2016-11-07 10:18:39 -0800

    Initital project files for Kiddo

---
+++ Kiddo/TimelineViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TimelineViewController.swift
+//  Kiddo
+//
+//  Created by Filiz Kurban on 11/7/16.
+//  Copyright © 2016 Filiz Kurban. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
