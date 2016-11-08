39f0e84073e5293b4a32c668dc6c23aa810f832b
John Shaff <johshaff@gmail.com>
2016-11-08 09:21:16 -0800

    Clean up commit

--- Kiddo/EventTableViewNib.swift
+++ Kiddo/EventTableViewNib.swift
@@ -15,28 +15,25 @@ class EventTableViewNib: UITableViewCell {
     
     @IBOutlet weak var eventTitle: UILabel!
     
-    @IBOutlet weak var eventLocation: UILabel!
+    @IBOutlet weak var eventVenueName: UILabel!
     
     @IBOutlet weak var eventDate: UILabel!
     
     
     @IBOutlet weak var eventPrice: UILabel!
     
-    @IBOutlet weak var eventTime: UILabel!
+    @IBOutlet weak var eventStartTime: UILabel!
     
-
+    //Everytime this class is instantiated
     var event: Event! {
         didSet {
             
-           // self.eventTitle.text = event.eventTitle
-//         self.eventImage.text = event.eventImage
-           //self.eventLocation.text = event.eventLocation
-            //self.eventDate.text = event.eventDate
-            //self.eventPrice.text = event.eventPrice
-            //self.eventTime.text = event.eventTime
-            
-            
-            
+            self.eventTitle.text = event.eventTitle
+//            self.eventImage.text = event.eventImage
+            self.eventVenueName.text = event.eventVenueName
+            self.eventDate.text = event.eventDate
+//            self.eventPrice.text = event.eventPrice
+//            self.eventStartTime.text = event.eventStartTime
             
             
         }
--- Kiddo/TimelineViewController.swift
+++ Kiddo/TimelineViewController.swift
@@ -13,10 +13,17 @@ class TimelineViewController: UIViewController {
 
     @IBOutlet weak var timelineTableView: UITableView!
 
-    var events = [Event]()
+    var events = [Event]() {
+        didSet {
+            self.timelineTableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.timelineTableView.dataSource = self
+        self.timelineTableView.delegate = self
 
         let nib = UINib(nibName: "eventCell", bundle: Bundle.main)
         self.timelineTableView.register(nib, forCellReuseIdentifier: EventTableViewNib.identifier())
@@ -29,7 +36,20 @@ class TimelineViewController: UIViewController {
         }
 
     }
+}
 
-
-
+extension TimelineViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.events.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.timelineTableView.dequeueReusableCell(withIdentifier: EventTableViewNib.identifier(), for: indexPath) as! EventTableViewNib
+        
+        let currentEvent = self.events[indexPath.row]
+        cell.event = currentEvent
+        
+        return cell
+    }
 }
